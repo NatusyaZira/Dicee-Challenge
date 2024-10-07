d1a0c3658972485855ed699440041089d4e67f17
Nataliia Kovalova <natusya.zira@outlook.com>
2024-10-07 19:12:38 +0300

    Throw dicee by random

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-prompt("What is your name?", "");
+let randomNumber1 = Math.floor(Math.random()*6)+1;
+let randomNumber2 = Math.floor(Math.random()*6)+1;
+const image1 = document.querySelector('.img1');
+const image2 = document.querySelector('.img2');
+
+image1.setAttribute('src', `images/dice${randomNumber1}.png`);
+image2.setAttribute('src', `images/dice${randomNumber2}.png`);
