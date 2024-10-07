225e8ea139abb96d9b48e8f59a6ceed45fa45bcb
Nataliia Kovalova <natusya.zira@outlook.com>
2024-10-07 19:27:20 +0300

    Make challenhe up and running

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ let randomNumber2 = Math.floor(Math.random()*6)+1;
 const image1 = document.querySelector('.img1');
 const image2 = document.querySelector('.img2');
 
+
+
 image1.setAttribute('src', `images/dice${randomNumber1}.png`);
 image2.setAttribute('src', `images/dice${randomNumber2}.png`);
+
+randomNumber1 > randomNumber2 ? document.querySelector('h1').innerHTML ='🚩Player 1 Wins!' 
+    :randomNumber2 > randomNumber1 ? document.querySelector('h1').innerHTML = 'Player 2 Wins! 🚩'
+    :document.querySelector('h1').innerHTML ='Draw';
+
